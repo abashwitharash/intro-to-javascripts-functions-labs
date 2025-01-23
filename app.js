f64a2806316d56fd50ff2d305a904f2452b6bfe5
Arash <arashsaleh798@gmail.com>
2025-01-23 11:49:03 -0800

    exercise 2 is complete

--- app.js
+++ app.js
@@ -17,4 +17,17 @@ const maxOfTwoNumbers = (x, y) => {
   }
   
   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
-  
+
+
+  //Exercise 2 
+
+
+function isAdult(number) {
+    if (number >= 18) {
+    return "Adult";}
+    else {
+        return "Minor";
+    }
+}
+
+    console.log('Exercise 2 Result:', isAdult(21));
