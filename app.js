234da96e9cb448f852dfeea2ed9ac3e373705812
Arash <arashsaleh798@gmail.com>
2025-01-23 12:20:39 -0800

    Exercise 5 is complete

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const maxOfTwoNumbers = (x, y) => {
   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 
 
-  //Exercise 2 
+  //Exercise 2 isAdult 
 function isAdult(number) {
     if (number >= 18) {
         return "Adult";
@@ -29,7 +29,7 @@ function isAdult(number) {
 }
 console.log('Exercise 2 Result:', isAdult(21));
 
-// Exercise 3 Vowels are the letters A, E, I, O, U
+// Exercise 3 - Vowels are the letters A, E, I, O, U
 
 function isCharAVowel(character) {
  if (character === "a" || character === "e" || character === "i" || 
@@ -40,10 +40,24 @@ function isCharAVowel(character) {
     }
     console.log('Exercise 3 Result:', isCharAVowel("a"));
 
-// Exercise 4
+// Exercise 4 generate email
 
 function generateEmail(fullName, emailAddress) {
     return `${fullName}${emailAddress}`;
 }
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+//Exercise 5 greetuser 
+
+function greetUser(Name, timeOfDay) {
+    if (timeOfDay === "morning") {
+        return `Good ${timeOfDay}, ${Name}`;
+    } else if (timeOfDay === "afternoon") {
+        return `Good ${timeOfDay}, ${Name}`;
+    } else {
+        return `Good evening, ${Name}`
+    }
+    }
+    console.log('Exercise 5 Result:', greetUser("Sam", "afternoon"));
+
